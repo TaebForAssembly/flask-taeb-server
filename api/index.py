1af00701b371b3fd6f2eddcb82227f77c38b4803
Jack Kraus <jackkraus3856@gmail.com>
2025-05-06 21:17:24 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,39 +1,34 @@
 import os
 from flask import Flask
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev'
-    )
+# create and configure the app
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_mapping(
+    SECRET_KEY='dev'
+)
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+if test_config is None:
+    # load the instance config, if it exists, when not testing
+    app.config.from_pyfile('config.py', silent=True)
+else:
+    # load the test config if passed in
+    app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
 
-    from . import auth
-    app.register_blueprint(auth.bp)
+from . import auth
+app.register_blueprint(auth.bp)
 
-    from . import mailing_list
-    app.register_blueprint(mailing_list.bp)
+from . import mailing_list
+app.register_blueprint(mailing_list.bp)
 
-    from . import embed
-    app.register_blueprint(embed.bp)
+from . import embed
+app.register_blueprint(embed.bp)
 
-    @app.route('/', methods=["GET"])
-    def hello_world():
-        return "Hello World"
-
-    return app
-
-app = create_app()
+@app.route('/', methods=["GET"])
+def hello_world():
+    return "Hello World"
