ad1584a6e23abe4c288fc2100ab7e4610d24cde1
Jack Kraus <jackkraus3856@gmail.com>
2025-05-06 20:55:50 -0400

    Feat: Initialization

--- api/index.py
+++ api/index.py
@@ -1,11 +1,37 @@
+import os
 from flask import Flask
 
-app = Flask(__name__)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-@app.route('/about')
-def about():
-    return 'About'
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+
+    from . import mailing_list
+    app.register_blueprint(mailing_list.bp)
+
+    from . import embed
+    app.register_blueprint(embed.bp)
+
+    @app.route('/', methods=["GET"])
+    def hello_world():
+        return "Hello World!"
+
+    return app
