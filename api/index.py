68ba8f86854d695367bcfe900150db52ac6a0e67
Jack Kraus <jackkraus3856@gmail.com>
2025-05-09 17:19:36 -0400

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,24 +2,36 @@ import os
 from flask import Flask
 from . import auth, mailing_list, embed
 
-# create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev',
-)
-app.config.from_pyfile('config.py', silent=True)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-app.register_blueprint(auth.bp)
-app.register_blueprint(mailing_list.bp)
-app.register_blueprint(embed.bp)
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-# a simple page that says hello
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(mailing_list.bp)
+    app.register_blueprint(embed.bp)
+
+    # a simple page that says hello
+    @app.route('/')
+    def root():
+        return 'Hello, World! Server working'
+
+    return app
+
+app = create_app()
