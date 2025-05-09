b5d6207fd1934112984ec36c5cb57daf01f819eb
Jack Kraus <jackkraus3856@gmail.com>
2025-05-09 17:03:57 -0400

    Fix local imports

--- api/index.py
+++ api/index.py
@@ -1,35 +1,41 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask
+import auth, mailing_list, embed
 
-# create and configure the app
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_mapping(
-    SECRET_KEY='dev'
-)
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+    )
 
-test_config = None
-if test_config is None:
-    # load the instance config, if it exists, when not testing
-    app.config.from_pyfile('config.py', silent=True)
-else:
-    # load the test config if passed in
-    app.config.from_mapping(test_config)
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
 
-# ensure the instance folder exists
-try:
-    os.makedirs(app.instance_path)
-except OSError:
-    pass
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
-from . import auth
-app.register_blueprint(auth.bp)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(mailing_list.bp)
+    app.register_blueprint(embed.bp)
 
-from . import mailing_list
-app.register_blueprint(mailing_list.bp)
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
 
-from . import embed
-app.register_blueprint(embed.bp)
+    return app
 
-@app.route('/', methods=["GET"])
-def hello_world():
-    return "Hello World"
+app = create_app()
+
+if __name__ == '__main__':
+    load_dotenv()
+    app.run(debug=True)
